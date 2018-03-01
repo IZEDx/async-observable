62dbd976ec6ae2623b3fc0b414a46ba6acbdf042
Niklas Kühtmann <admin@i-zed.net>
2018-03-01 17:01:08 +0100

    Removing dist to remove sourcemaps

--- dist/esnext/operators/index.js
+++
@@ -1,8 +0,0 @@
-import * as Generators from "./generators";
-import * as Filters from "./filters";
-import * as Aggregators from "./aggregators";
-import * as Transformators from "./transformators";
-import * as Utilities from "./utilities";
-const Operators = { ...Filters, ...Aggregators, ...Transformators, ...Utilities };
-export { Generators, Operators, Filters, Aggregators, Transformators, Utilities };
-//# sourceMappingURL=index.js.map
