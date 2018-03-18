04ccd0e8faae91d3a35f9f7d1d4e4fa8720e1887
Niklas Kühtmann <admin@i-zed.net>
2018-03-19 00:34:10 +0100

    Updated signatures of min and max

--- src/operators/aggregators.ts
+++ src/operators/aggregators.ts
@@ -1,6 +1,7 @@
 
 import { Filters } from "./";
 
+    
 export async function* count<T>(input: AsyncIterable<T>, predicate?: (value: T) => Promise<boolean>|boolean): AsyncIterable<number> {
     let c = 0;
     for await(const _ of !!predicate ? Filters.filter(input, predicate) : input) {
@@ -9,22 +10,21 @@ export async function* count<T>(input: AsyncIterable<T>, predicate?: (value: T) 
     yield c;
 }
 
-export async function* max<T = number>(input: AsyncIterable<T>, comparer?: (a: T, b: T) => number): AsyncIterable<T> {
-    if (!comparer) {
-        comparer = (a: T, b: T) => {
-            if (typeof a !== "number" || typeof b !== "number") {
-                throw TypeError("Input must be number when no comparer is given.");
-            }
-            return a > b ? 1 : -1;
-        }
-    }
+export type CompareFunction<T> = (a: T, b: T) => 1|0|-1;
+export type AssumeCompareType<T, U> = T extends U ? CompareFunction<U>|undefined : CompareFunction<T>;
+const numberComparer: CompareFunction<number> = (a: number, b: number) => a > b ? 1 : -1;
+
+export function max<T extends number>(input: AsyncIterable<T>, comparer?: CompareFunction<T>): AsyncIterable<T>;
+export function max<T>(input: AsyncIterable<T>, comparer: CompareFunction<T>): AsyncIterable<T>
+export async function* max<T>(input: AsyncIterable<T>, comparer: AssumeCompareType<T, number>): AsyncIterable<T> {
+    let comp = <CompareFunction<T>> (comparer || numberComparer);
 
     let max: T|null = null;
     for await(const val of input) {
         if (max === null) {
             max = val;
         } else {
-            max = comparer(val, max) > 0 ? val : max;
+            max = comp(val, max) > 0 ? val : max;
         }
     }
     if (max !== null) {
@@ -32,22 +32,14 @@ export async function* max<T = number>(input: AsyncIterable<T>, comparer?: (a: T
     }
 }
 
-export async function* min<T = number>(input: AsyncIterable<T>, comparer?: (a: T, b: T) => number): AsyncIterable<T> {
-    if (!comparer) {
-        comparer = (a: T, b: T) => {
-            if (typeof a !== "number" || typeof b !== "number") {
-                throw TypeError("Input must be number when no comparer is given.");
-            }
-            return a < b ? 1 : -1;
-        }
-    } else {
-        const origcomp = comparer;
-        comparer = (a: T, b: T) => {
-            return origcomp(a, b) < 0 ? 1 : -1;
-        }
-    }
+export function min<T extends number>(input: AsyncIterable<T>, comparer?: CompareFunction<T>): AsyncIterable<T>;
+export function min<T>(input: AsyncIterable<T>, comparer: CompareFunction<T>): AsyncIterable<T>
+export async function* min<T>(input: AsyncIterable<T>, comparer: AssumeCompareType<T, number>): AsyncIterable<T> {
+    let comp: CompareFunction<T> = <any>comparer || <any>numberComparer;
 
-    return max(input, comparer);
+    return max(input, (a: T, b: T) => {
+        return comp(a,b) < 0 ? 1 : -1;
+    });
 }
 
 export async function* reduce<T, K = T>(input: AsyncIterable<T>, fn: (acc: K, curr: T) => K, seed: K): AsyncIterable<K> {
--- types/operators/aggregators.d.ts
+++ types/operators/aggregators.d.ts
@@ -1,4 +1,8 @@
 export declare function count<T>(input: AsyncIterable<T>, predicate?: (value: T) => Promise<boolean> | boolean): AsyncIterable<number>;
-export declare function max<T = number>(input: AsyncIterable<T>, comparer?: (a: T, b: T) => number): AsyncIterable<T>;
-export declare function min<T = number>(input: AsyncIterable<T>, comparer?: (a: T, b: T) => number): AsyncIterable<T>;
+export declare type CompareFunction<T> = (a: T, b: T) => 1 | 0 | -1;
+export declare type AssumeCompareType<T, U> = T extends U ? CompareFunction<U> | undefined : CompareFunction<T>;
+export declare function max<T extends number>(input: AsyncIterable<T>, comparer?: CompareFunction<T>): AsyncIterable<T>;
+export declare function max<T>(input: AsyncIterable<T>, comparer: CompareFunction<T>): AsyncIterable<T>;
+export declare function min<T extends number>(input: AsyncIterable<T>, comparer?: CompareFunction<T>): AsyncIterable<T>;
+export declare function min<T>(input: AsyncIterable<T>, comparer: CompareFunction<T>): AsyncIterable<T>;
 export declare function reduce<T, K = T>(input: AsyncIterable<T>, fn: (acc: K, curr: T) => K, seed: K): AsyncIterable<K>;
