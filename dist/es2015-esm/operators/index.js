07b452af166dced4b2a403ae45dd3420dadf96ca
Niklas Kühtmann <admin@i-zed.net>
2018-04-04 19:46:34 +0200

    Removed dist from repository

--- dist/es2015-esm/operators/index.js
+++
@@ -1,8 +0,0 @@
-import * as Generators from "./generators";
-import * as Filters from "./filters";
-import * as Aggregators from "./aggregators";
-import * as Transformators from "./transformators";
-import * as Utilities from "./utilities";
-const Operators = Object.assign({}, Filters, Aggregators, Transformators, Utilities);
-export { Generators, Operators, Filters, Aggregators, Transformators, Utilities };
-//# sourceMappingURL=index.js.map
--- dist/esnext/operators/filters.js
+++
@@ -1,9 +0,0 @@
-export async function* filter(input, fn) {
-    for await (const data of input) {
-        const check = fn(data);
-        if (check instanceof Promise ? await check : check) {
-            yield data;
-        }
-    }
-}
-//# sourceMappingURL=filters.js.map
