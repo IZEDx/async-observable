d7345c8a62e80c024f04f9c50f28bc97ba586418
Niklas Kühtmann <admin@i-zed.net>
2018-04-10 00:24:51 +0200

    test

--- src/debug.ts
+++ src/debug.ts
@@ -1,5 +1,5 @@
 import { Observable } from ".";
-
+/*
 (async () => {
     const it = Observable.create(observer => {
         observer.next(0);
@@ -22,3 +22,18 @@ int().map(x => 10 - x).subscribe(printer("10-x"));
 const sub = int().subscribe(printer("x"));
 
 Observable.duration(1200).subscribe({next: () => {}, return: sub.unsubscribe});
+*/
+
+const UPDATE_MIN = 0.01 * 60 * 1000;
+const UPDATE_MAX = 0.1 * 60 * 1000;
+export const sleep = (ms: number) => new Promise(res => setTimeout(res, ms));
+
+async function test() {
+    while (true) {
+        const sleepFor = Math.floor(Math.random() * (UPDATE_MAX - UPDATE_MIN + 1) + UPDATE_MIN);
+        console.log(sleepFor)
+        await sleep(sleepFor);
+    }
+}
+
+test();
